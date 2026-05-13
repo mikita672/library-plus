e2364763a2e5528910feef7cee46f42236e2d4bc
justkinou <justkinou@proton.me>
2026-05-14 01:35:35 +0200

    add search bar

---
+++ frontend/app/catalog/page.tsx
@@ -0,0 +1,12 @@
+import SearchBar from '@/components/catalog/SearchBar'
+import React from 'react'
+
+function Catalog() {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
+
+export default Catalog
---
+++ frontend/components/catalog/SearchBar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import React, { useState } from 'react'
+import { InputGroup, InputGroupAddon, InputGroupInput } from '../ui/input-group';
+import { MagnifyingGlassIcon } from '@phosphor-icons/react';
+import { Button } from '../ui/button';
+
+function SearchBar() {
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    const [searchToken, setSearchToken] = useState(searchParams.get("searchToken") ?? "");
+
+    const handleSearch = async () => {
+        const params = new URLSearchParams(searchParams);
+
+        if (searchToken.length === 0) {
+            params.delete("searchToken");
+        } else {
+            params.set("searchToken", searchToken);
+        }
+
+        router.replace(`${pathname}?${params.toString()}`);
+    }
+
+    return (
+        <InputGroup className="py-5">
+            <InputGroupInput
+                placeholder="Search books by name"
+                value={searchToken}
+                onChange={(e) => setSearchToken(e.target.value.trim())}
+            />
+
+            <InputGroupAddon>
+                <MagnifyingGlassIcon />
+            </InputGroupAddon>
+
+            <InputGroupAddon align="inline-end">
+                <Button
+                    className="cursor-pointer px-6"
+                    onClick={handleSearch}
+                >Search</Button>
+            </InputGroupAddon>
+        </InputGroup>
+    )
+}
+
+export default SearchBar
