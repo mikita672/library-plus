d249cd23d223de8a9dc04eaf7175f5a3a81c0ad3
justkinou <justkinou@proton.me>
2026-06-06 12:56:06 +0200

    add cookies policy page

---
+++ frontend/app/(misc)/cookies-policy/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function CookiesPolicy() {
+    return (
+        <>
+            <b className="text-xl">What are cookies</b>
+            <p>Cookies are simple text files that are stored on your computer or mobile device by a website’s
+                server. Each cookie is unique to your web browser. It will contain some anonymous information
+                such as a unique identifier, website’s domain name, and some digits and numbers</p>
+            <b className="text-xl">What types of cookies do we use?</b>
+            <p className="text-lg">Necessary cookies</p>
+            <p>Necessary cookies allow us to offer you the best possible experience when accessing and
+                navigating through our website and using its features. For example, these cookies let us
+                recognize that you have created an account and have logged into that account.</p>
+            <b className="text-xl">How to delete cookies?</b>
+            <p>If you want to restrict or block the cookies that are set by our website, you can do so through
+                your browser setting. Alternatively, you can visit www.internetcookies.com, which contains
+                comprehensive information on how to do this on a wide variety of browsers and devices. You
+                will find general information about cookies and details on how to delete cookies from your
+                device.</p>
+        </>
+    )
+}
+
+export default CookiesPolicy
