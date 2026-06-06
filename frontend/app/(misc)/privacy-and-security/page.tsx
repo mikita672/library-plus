00aae71cc0ea937eb9f8381bb3e39321e14764f0
justkinou <justkinou@proton.me>
2026-06-06 12:52:20 +0200

    add privacy and security page

---
+++ frontend/app/(misc)/privacy-and-security/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PrivacyAndSecurity() {
+    return (
+        <>
+            <p>We stores user data on our servers</p>
+            <p>Library+ will provide the privacy and security of user data to its best ability</p>
+        </>
+    )
+}
+
+export default PrivacyAndSecurity
