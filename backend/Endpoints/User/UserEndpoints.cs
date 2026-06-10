02ce6e49c61018e69cfe4e1d51b766f5e7c02805
Mikita Dzeviatau <9mikita@gmail.com>
2026-06-10 22:48:11 +0200

    hotfix user profile images on the admin panel

--- backend/Endpoints/User/UserEndpoints.cs
+++ backend/Endpoints/User/UserEndpoints.cs
@@ -19,27 +19,33 @@ public static class UserEndpoints
 
         group.MapGet("/meShort", [Authorize] async (ClaimsPrincipal claims, UserService userService) =>
         {
-            var userId = int.Parse(claims.FindFirstValue("sub")!);
-            var user = (await userService.GetUserById(userId))!;
-            return new MeShortResponse(user.Email, user.Name, userService.GetAvatarUrlById(user), user.PhoneNumber);
+            var userIdStr = claims.FindFirstValue("sub");
+            if (string.IsNullOrEmpty(userIdStr) || !int.TryParse(userIdStr, out var userId)) return Results.Unauthorized();
+            
+            var user = await userService.GetUserById(userId);
+            if (user == null) return Results.NotFound();
+            
+            return Results.Ok(new MeShortResponse(user.Email, user.Name, userService.GetAvatarUrlById(user), user.PhoneNumber));
         });
 
         group.MapGet("/user/{id}", [Authorize] async (int id, UserService userService) =>
         {
             var user = await userService.GetUserById(id);
-            if (user == null)
-            {
-                return Results.NotFound();
-            }
+            if (user == null) return Results.NotFound();
+            
             return Results.Ok(new MeShortResponse(user.Email, user.Name, userService.GetAvatarUrlById(user), user.PhoneNumber));
         });
 
         group.MapGet("/me", [Authorize] async (ClaimsPrincipal claims, UserService userService) =>
         {
-            var userId = int.Parse(claims.FindFirstValue("sub")!);
-            var user = (await userService.GetUserById(userId))!;
+            var userIdStr = claims.FindFirstValue("sub");
+            if (string.IsNullOrEmpty(userIdStr) || !int.TryParse(userIdStr, out var userId)) return Results.Unauthorized();
+
+            var user = await userService.GetUserById(userId);
+            if (user == null) return Results.NotFound();
+
             var response = MeResponse.FromModel(user);
-            return response with { AvatarUrl = userService.GetAvatarUrlById(user) };
+            return Results.Ok(response with { AvatarUrl = userService.GetAvatarUrlById(user) });
         });
 
         group.MapPatch("/updatePhoneNumber", [Authorize] async (
